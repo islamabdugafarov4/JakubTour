d0c46a7c6980db13733676b27bf3c83d528c63b1
islam <islam@docchain.kz>
2019-03-06 00:49:34 +0600

    добавлена главная страница, установлена отправка на почту с помощью кнолпки

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import {EmailComposer} from "@ionic-native/email-composer/ngx";
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+constructor(private emailComposer:EmailComposer){}
+
+
+
+  sendEmail(){
+    let email = {
+      to: 'esendemail@bk.ru',
+      cc:'abdugafarov.islam@mail.ru',
+      attachments:[
+          //send files
+      ],
+        subject:'Hello',
+        body:'dasdasdas',
+        isHtml:true
+    };
+    this.emailComposer.open(email);
+    }
+
+}
