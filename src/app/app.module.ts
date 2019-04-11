8d5866d0ca3ff79af1cb6e302a431c4186136228
islamabdugafarov4 <islamabdugafarov4@gmail.com>
2019-04-11 15:41:03 +0600

    добавлена маска

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { BrMaskerModule } from 'brmasker-ionic-3';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -13,7 +13,9 @@ import {EmailComposer} from "@ionic-native/email-composer/ngx";
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+      BrMaskerModule,
+      BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/send-data/send-data.module.ts
+++ src/app/send-data/send-data.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { SendDataPage } from './send-data.page';
+import {BrMaskerModule} from "brmasker-ionic-3";
 
 const routes: Routes = [
   {
@@ -19,7 +20,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+      BrMaskerModule
   ],
   declarations: [SendDataPage]
 })
